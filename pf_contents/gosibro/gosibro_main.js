c7490360ef05aec804e3b0ef997b1869ba0f890c
Youngmin Kho <ihopeirum@gmail.com>
2020-09-29 19:35:01 +0900

    modified JS

--- pf_contents/gosibro/gosibro_main.js
+++ pf_contents/gosibro/gosibro_main.js
@@ -1,34 +1,49 @@
 window.onload = function () {
 
   var main_bg = document.querySelector('.mainWrap')
-
-  // var bt_changeBg = document.querySelectorAll('.mainMenu')
-  // var current;
-
   var bt_weAre = document.getElementById('weAre')
   var bt_nature = document.getElementById('nature')
   var bt_culture = document.getElementById('culture')
   var bt_diary = document.getElementById('diary')
 
+  // var bt_changeBg = document.querySelectorAll('.mainMenu')
+
+  // for(i=0; i<bt_changeBg.length; i++){
+  //   bt_changeBg[i].onmouseover = function mouseoverHandler() {
+  //     if (main_bg) {
+  //       main_bg.classList.remove('mainWrap_0','mainWrap_1','mainWrap_2','mainWrap_3');
+  //     }
+  //     main_bg.classList.add('mainWrap_[i]');
+  //     main_bg = bt_changeBg;
+  //   }
+  // }
+  // 콘솔 에러메시지도 안뜸... 왜 안되는 걸까.
+
   bt_weAre.onmouseover = function weAre(){
+    main_bg.classList.remove('mainWrap_nature');
+    main_bg.classList.remove('mainWrap_culture');
+    main_bg.classList.remove('mainWrap_diary');
     main_bg.classList.add('mainWrap_weAre');
   }
 
   bt_nature.onmouseover = function nature() {
+    main_bg.classList.remove('mainWrap_weAre');
+    main_bg.classList.remove('mainWrap_culture');
+    main_bg.classList.remove('mainWrap_diary');
     main_bg.classList.add('mainWrap_nature');
   }
 
   bt_culture.onmouseover = function culture() {
+    main_bg.classList.remove('mainWrap_nature');
+    main_bg.classList.remove('mainWrap_weAre');
+    main_bg.classList.remove('mainWrap_diary');
     main_bg.classList.add('mainWrap_culture');
   }
 
   bt_diary.onmouseover = function diary() {
+    main_bg.classList.remove('mainWrap_nature');
+    main_bg.classList.remove('mainWrap_culture');
+    main_bg.classList.remove('mainWrap_weAre');
     main_bg.classList.add('mainWrap_diary');
   }
-
-  // 현재상태 : div 1-2-3-4 순으로 1회만 성공. 되돌아오기 안됨
-  // current 외 다른 클라스 제거하기
-  // 함수 최소화
-
-
 }
